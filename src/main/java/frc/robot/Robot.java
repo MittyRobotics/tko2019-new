6df2604362db1ec63eb5edcd13e7cec7deed2f54
programming1351 <programming@amhsrobotics.com>
2019-06-02 14:27:40 -0700

    Adding files

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,39 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends TimedRobot {
+
+  @Override
+  public void robotInit() {
+    
+  }
+
+  @Override
+  public void robotPeriodic() {
+  
+  }
+
+  @Override
+  public void autonomousInit() {
+   
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    
+  }
+
+  @Override
+  public void teleopPeriodic() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+}
