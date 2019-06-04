dfd01c980d979e2902c19d6810d8845651d0148f
Owen Leather <owenleather22@mittymonarch.com>
2019-06-04 16:09:02 -0700

    Filled in rotation and translation commands with DriveTrain functions

--- src/main/java/frc/robot/autonomous/movement/commands/Rotation.java
+++ src/main/java/frc/robot/autonomous/movement/commands/Rotation.java
@@ -2,6 +2,7 @@ package frc.robot.autonomous.movement.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.drive.DriveTrain;
+import frc.robot.drive.constants.TicksPerInch;
 
 /**
  * Rotation command for rotating the robot's chassis a set amount of degrees using a PID control loop.
@@ -13,38 +14,48 @@ public class Rotation extends Command {
 	double angle; //degrees
 	double maxSpeed; //percent output
 	RotationDirection direction; //direction of rotation
+	double stoppingThreshold = 1; //Rotation error threshold, the robot will stop rotating when within this threshold of degrees
 	
 	/**
-	 * Initializes command
+	 * Initializes command with a name "Rotation" and the subsystem class that will be used, {@link DriveTrain}
 	 *
 	 * @param angle     the angle at which the robot will be rotating
 	 * @param maxSpeed  the maximum speed at which the robot will be rotating at
 	 * @param direction the direction in which the robot will be rotating
 	 */
 	public Rotation(double angle, double maxSpeed, RotationDirection direction) {
+		
 		super("Rotation");
 		requires(DriveTrain.getInstance());
+		
+		if(direction == RotationDirection.Left){
+			angle = -angle;
+		}
 		this.angle = angle;
 		this.maxSpeed = maxSpeed;
-		this.direction = direction;
 	}
 	
 	/**
-	 * The initialize function is called at the initialization stage of the command. This is where the
-	 * drive PID control loop values are set.
+	 * The initialize function is called at the initialization stage of the command.
+	 *
+	 * This is where the rotation PID control loop values are set. The rotation method in the {@link DriveTrain}
+	 * subsystem is called with the parameters of angle and max speed to rotate the robot the desired amount.
 	 */
 	@Override
 	public void initialize() {
-		//"Everybody needs a friend" -Bob Ross
+		DriveTrain.getInstance().rotation(angle,maxSpeed);
 	}
 	
 	/**
 	 * This method is called periodically (about every 20ms) and does the work of the command.
+	 *
+	 * All the command's work is to set the setpoint of the PID loop, so nothing is needed here.
 	 */
 	@Override
 	public void execute() {
 		//"Everybody needs a friend" -Bob Ross
 	}
+	
 	
 	/**
 	 * This method is called at the end of the command.
@@ -56,7 +67,7 @@ public class Rotation extends Command {
 	
 	
 	/**
-	 * This method is called when the command is interrupted
+	 * This method is called when the command is interrupted, therefore ending the command.
 	 */
 	@Override
 	public void interrupted() {
@@ -64,11 +75,17 @@ public class Rotation extends Command {
 	}
 	
 	/**
-	 * This returns whether or not the command is finished.
-	 * @return if the command is finished
+	 * This returns whether or not the command has finished, meaning the robot has rotated to its target angle.
+	 *
+	 * @return if the robot has rotated to its target angle
 	 */
 	@Override
 	protected boolean isFinished() {
-		return true;
+		if(Math.abs(DriveTrain.getInstance().getRotationError() / TicksPerInch.DRIVE) < stoppingThreshold ){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 }
--- src/main/java/frc/robot/autonomous/movement/commands/Translation.java
+++ src/main/java/frc/robot/autonomous/movement/commands/Translation.java
@@ -2,6 +2,7 @@ package frc.robot.autonomous.movement.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.drive.DriveTrain;
+import frc.robot.drive.constants.TicksPerInch;
 
 /**
  * Linear translation command for translating the robot either forward or backward on the local forward axis. Since the
@@ -11,7 +12,14 @@ public class Translation extends Command {
 	
 	private double distance; //inches
 	private double maxSpeed; //percent output
+	private double stoppingThreshold = 0.5; //Translation error threshold, the robot will stop translating when within this threshold of inches
 	
+	/**
+	 * Initializes command with a name "Translation" and the subsystem class that will be used, {@link DriveTrain}
+	 *
+	 * @param distance the distance that the robot will be moving
+	 * @param maxSpeed the maximum speed at which the robot will be moving at
+	 */
 	public Translation(double distance, double maxSpeed) {
 		super("Translation");
 		requires(DriveTrain.getInstance());
@@ -19,28 +27,52 @@ public class Translation extends Command {
 		this.maxSpeed = maxSpeed;
 	}
 	
+	/**
+	 * The initialize function is called at the initialization stage of the command. This is where the
+	 * translation PID control loop values are set. The translation method in the {@link DriveTrain} subsystem is called with the
+	 * parameters of distance and max speed to translate the robot the desired amount.
+	 */
 	@Override
 	public void initialize() {
-	
+		DriveTrain.getInstance().translation(distance, maxSpeed);
 	}
 	
+	/**
+	 * This method is called periodically (about every 20ms) and does the work of the command. All the command's work
+	 * is to set the setpoint of the PID loop, so nothing is needed here.
+	 */
 	@Override
 	public void execute() {
 		//"Everybody needs a friend" -Bob Ross
 	}
 	
+	/**
+	 * This method is called at the end of the command.
+	 */
 	@Override
 	public void end() {
 		//"Everybody needs a friend" -Bob Ross
 	}
 	
+	/**
+	 * This method is called when the command is interrupted, therefore ending the command.
+	 */
 	@Override
 	public void interrupted() {
 		end();
 	}
 	
+	/**
+	 * This returns whether or not the command has finished, meaning the robot has translated to its destination.
+	 * @return if the robot has translated to its destination
+	 */
 	@Override
 	protected boolean isFinished() {
-		return false;
+		if(Math.abs(DriveTrain.getInstance().getTranslationError() / TicksPerInch.DRIVE) < stoppingThreshold ){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 }
