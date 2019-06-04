04bdcdd6fa1a279e51be2e8cc6b90d9af0083114
Owen Leather <owenleather22@mittymonarch.com>
2019-06-03 21:17:14 -0700

    Vision constants

---
+++ src/main/java/frc/robot/autonomous/constants/VisionConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.autonomous.constants;
+
+public class VisionConstants {
+    public final double FOCAL_LENGTH = 3.3385168390258093; //mm
+    public final double H_FOV = 59.6; //degrees
+    public final double V_FOV = 49.7; //degrees
+
+    public final int AREA_TO_STOP = 30; //% of screen TODO: CALIBRATE
+}
--- src/main/java/frc/robot/autonomous/vision/Limelight.java
+++ src/main/java/frc/robot/autonomous/vision/Limelight.java
@@ -22,22 +22,22 @@ public class Limelight {
         //Lonliness
     }
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //                                       Limelight Network Tables API:                                              //
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //tv	Whether the limelight has any valid targets (0 or 1)                                                        //
-    //tx	Horizontal Offset From Crosshair To Target (LL1: -27 degrees to 27 degrees | LL2: -29.8 to 29.8 degrees)    //
-    //ty	Vertical Offset From Crosshair To Target (LL1: -20.5 degrees to 20.5 degrees | LL2: -24.85 to 24.85 degrees)//
-    //ta	Target Area (0% of image to 100% of image)                                                                  //
-    //ts	Skew or rotation (-90 degrees to 0 degrees)                                                                 //
-    //tl	The pipeline’s latency contribution (ms) Add at least 11ms for image capture latency.                       //
-    //tshort	Sidelength of shortest side of the fitted bounding box (pixels)                                         //
-    //tlong	Sidelength of longest side of the fitted bounding box (pixels)                                              //
-    //thor	Horizontal sidelength of the rough bounding box (0 - 320 pixels)                                            //
-    //tvert	Vertical sidelength of the rough bounding box (0 - 320 pixels)                                              //
-    //getpipe	True active pipeline index of the camera (0 .. 9)                                                       //
-    //camtran	Results of a 3D position solution, 6 numbers: Translation (x,y,y) Rotation(pitch,yaw,roll)              //
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                       Limelight Network Tables API:                                //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //tv	Whether the limelight has any valid targets (0 or 1)                                          //
+    //tx	Horizontal Offset From Crosshair To Target (LL2: -29.8 to 29.8 degrees)                       //
+    //ty	Vertical Offset From Crosshair To Target (LL2: -24.85 to 24.85 degrees)                       //
+    //ta	Target Area (0% of image to 100% of image)                                                    //
+    //ts	Skew or rotation (-90 degrees to 0 degrees)                                                   //
+    //tl	The pipeline’s latency contribution (ms) Add at least 11ms for image capture latency.         //
+    //tshort	Sidelength of shortest side of the fitted bounding box (pixels)                           //
+    //tlong	Sidelength of longest side of the fitted bounding box (pixels)                                //
+    //thor	Horizontal sidelength of the rough bounding box (0 - 320 pixels)                              //
+    //tvert	Vertical sidelength of the rough bounding box (0 - 320 pixels)                                //
+    //getpipe	True active pipeline index of the camera (0 .. 9)                                         //
+    //camtran	Results of a 3D position solution, 6 numbers: Translation (x,y,y) Rotation(pitch,yaw,roll)//
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     //Update the limelight values from network tables
     void updateLimelightValues(){
