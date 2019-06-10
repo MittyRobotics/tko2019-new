2766bb571af9a679da9874d78a02949c341828e9
Owen Leather <owenleather22@mittymonarch.com>
2019-06-09 19:15:15 -0700

    updated comments on VisionAlignment.java

--- src/main/java/frc/robot/autonomous/movement/commands/VisionAlignment.java
+++ src/main/java/frc/robot/autonomous/movement/commands/VisionAlignment.java
@@ -58,44 +58,67 @@ public class VisionAlignment extends Command {
 	@Override
 	public void execute() {
 		//Get values from limelight
-		double distance = Limelight.getInstance().getCamZ();
-		double xOffset = Limelight.getInstance().getCamX();
-		double skew = Limelight.getInstance().getCamYaw();
-		double angle = Limelight.getInstance().getXAngle();
+		double distance = Limelight.getInstance().getCamZ(); //Distance from robot to target (perpendicular to target)
+		double xOffset = Limelight.getInstance().getCamX(); //Perpendicular distance from perpendicular distance to target to robot
+		double skew = Limelight.getInstance().getCamYaw(); //Robot's rotation relative to target (target rotation is at (0,0,0))
+		double angle = Limelight.getInstance().getXAngle(); //X angle to target relative to robot's forward vector
 
+		//Measurement Graph:
+		//     ------   <-target
+		//       |
+		//       |   <- distance
+		//       |
+		//       |--------[] <- robot
+		//         ^x Offset
 
 		int SLOW_RANGE = 3; //feet
 
-		////////Start Math////////
+		//Math for calculating new target angle at stoppingDistance away from the target
+		//TODO: IDK if the angle standards (left is negative, angle starts at 0 relative to target, etc.) on Limelight are how I assumed they are, so I will experiment with the limelight and correct the math if needed
+
+		//Direct distance to target (hypotenuse)
 		double d = Math.sqrt((xOffset * xOffset) + (distance * distance));
 
+		//New distance that the robot should target
 		double newDistance = distance - stoppingDistance;
 
+		//Direct distance to new stopping point
 		double d1 = Math.sqrt((xOffset * xOffset) + ((newDistance) * (newDistance)));
 
+		//Value for calculating other values that is hard to explain in comments
 		double x = d * Math.sin(Math.toRadians(angle));
 
+		//Value for calculating other values that is hard to explain in comments
 		double x1 = stoppingDistance * Math.sin(Math.toRadians(skew));
 
+		//Value for calculating other values that is hard to explain in comments
 		double x2 = x1 + x;
 
+		//Final angle value to new stopping point
 		double a2 = Math.toDegrees(Math.asin(x2 / d1));
 
+		//Set angle to negative if robot is on other side
 		if (xOffset < 0 || angle < 0) {
 			a2 = -a2;
 		}
-		////////End Math////////
 
-		////////Start Movement Calculations////////
+		//Calculations for drive and turn values
 		double gyroAngle = Gyro.getInstance().getAngle();
 
 		double drive;
-		double turn = 0;
+		double turn;
 
+		//Calculate drive speed based on distance divided by the slow range in inches, if robot is further from slow
+		// range it defaults to max speed and if robot is within slow range it starts to slow down based on how
+		// far it is from the target
 		drive = Math.min((newDistance / (12 * SLOW_RANGE)) * maxDriveSpeed, maxDriveSpeed);
 
+		//Add skew value to final angle setpoint, this will make the robot curve into the target and eventually be
+		// perpendicular to it, otherwise the robot runs straight into the target no matter what rotation the target is
+		// relative to the robot
 		double finalAngleSetpoint = a2 + skew; //TODO: Add variable ratio if needed
 
+		//Correct angle values based on gyro value going from 0-360
 		if (gyroAngle + finalAngleSetpoint < 0) {
 			gyroAngle = gyroAngle + finalAngleSetpoint + 360;
 		} else if (gyroAngle + finalAngleSetpoint >= 360) {
@@ -104,14 +127,17 @@ public class VisionAlignment extends Command {
 			gyroAngle = gyroAngle + finalAngleSetpoint;
 		}
 
+		//Set setpoint of PID controller
 		turnController.setSetpoint(gyroAngle);
 
+		//Set turn value to output of PID controller
 		turn = turnController.get(); //Get latest calculated output
-		/////////End Movement Calculations////////
 
+		//Move left and right wheels based on turn and drive values
 		DriveTrain.getInstance().tankDrive(drive + turn, drive - turn);
 
-		if (Math.abs(angle) <= 1 && newDistance <= 1) {
+		//Check if robot is positioned correctly infront of target, if so start a counter that is used to end the command
+		if (Math.abs(angle) <= 1 && newDistance <= 1) { //TODO: Tune thresholds
 			count++;
 		}
 	}
