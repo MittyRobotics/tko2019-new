1a75b50386f09d31524db08f3af7bf46726ab3c2
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2019-06-13 16:36:13 -0700

    added to subsystems

--- src/main/java/frc/robot/hatchpanel/Slider.java
+++ src/main/java/frc/robot/hatchpanel/Slider.java
@@ -1,4 +1,29 @@
 package frc.robot.hatchpanel;
 
-public class Slider {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Slider extends Subsystem {
+    private static Slider ourInstance = new Slider();
+    public static Slider getInstance() { return ourInstance; }
+
+    private Slider(){
+
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+    public void SliderAuton(){
+
+    }
+
+    public void SliderManual(){
+
+    }
 }
--- src/main/java/frc/robot/hatchpanel/Tilt.java
+++ src/main/java/frc/robot/hatchpanel/Tilt.java
@@ -1,4 +1,29 @@
 package frc.robot.hatchpanel;
 
-public class Tilt {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Tilt extends Subsystem {
+    private static Tilt ourInstance = new Tilt();
+    public static Tilt getInstance() { return ourInstance; }
+
+    private Tilt(){
+
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+    public void TiltBack(){
+
+    }
+
+    public void TiltForward(){
+
+    }
 }
