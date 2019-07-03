3c317813f0e5222f3b6f2fed346aae083d6b9fea
Natasha Lal <natashalal21@mittymonarch.com>
2019-07-02 21:26:06 -0700

    more setting up cargo

---
+++ src/main/java/frc/robot/cargo/constants/ArmPositions.java
@@ -0,0 +1,7 @@
+package frc.robot.cargo.constants;
+
+public class ArmPositions {
+    public static final double HIGH = 8;
+    public static final double MID = 4;
+    public static final double LOW = 2;
+}
---
+++ src/main/java/frc/robot/cargo/constants/PID.java
@@ -0,0 +1,5 @@
+package frc.robot.cargo.constants;
+
+public class PID {
+    public static final double[] SLIDER = {0.1, 0, 0};
+}
