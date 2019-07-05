1711a1524a296c46118be5920bdbe359fb79e24b
Owen Leather <owenleather22@mittymonarch.com>
2019-07-05 13:27:17 -0700

    Fixing imports
    all tko utils library are under team1351 package name now

--- src/main/java/frc/robot/oi/OI.java
+++ src/main/java/frc/robot/oi/OI.java
@@ -1,6 +1,5 @@
 package frc.robot.oi;
 
-import controlls.XboxWheel;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
@@ -19,6 +18,7 @@ import frc.robot.hatchpanel.commands.PushForward;
 import frc.robot.hatchpanel.commands.Release;
 import frc.robot.hatchpanel.commands.Slide;
 import frc.robot.hatchpanel.constants.SliderPosition;
+import team1351.commoncontrols.XboxWheel;
 
 
 public class OI {
