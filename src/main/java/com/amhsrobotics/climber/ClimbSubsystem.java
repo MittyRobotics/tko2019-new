387a4b5491a344870f8b3f4adfca02f76552b75f
SameerRao <sameerrao22@gmail.com>
2019-09-05 16:22:55 -0700

    limit switch added

--- src/main/java/com/amhsrobotics/climber/ClimbSubsystem.java
+++ src/main/java/com/amhsrobotics/climber/ClimbSubsystem.java
@@ -2,6 +2,7 @@ package com.amhsrobotics.climber;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class ClimbSubsystem extends Subsystem {
     private static ClimbSubsystem ourInstance = new ClimbSubsystem();
@@ -19,6 +20,10 @@ public class ClimbSubsystem extends Subsystem {
     }
     WPI_TalonSRX leftTalon = new WPI_TalonSRX(0);
     WPI_TalonSRX rightTalon = new WPI_TalonSRX(1);
+
+    DigitalInput limitSwitch;
+
+    limitSwitch = DigitalInput;
 
     public void resetEncoder(){
         leftTalon.setSelectedSensorPosition(0);
