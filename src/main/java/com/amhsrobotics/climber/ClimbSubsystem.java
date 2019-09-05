f041e99f1b2827ccfc9c80745602d87095ad33eb
SameerRao <sameerrao22@gmail.com>
2019-09-05 16:11:41 -0700

    Subsystem class

--- src/main/java/com/amhsrobotics/climber/ClimbSubsystem.java
+++ src/main/java/com/amhsrobotics/climber/ClimbSubsystem.java
@@ -1,5 +1,6 @@
 package com.amhsrobotics.climber;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class ClimbSubsystem extends Subsystem {
@@ -16,5 +17,11 @@ public class ClimbSubsystem extends Subsystem {
     protected void initDefaultCommand() {
 
     }
+    WPI_TalonSRX leftTalon = new WPI_TalonSRX(0);
+    WPI_TalonSRX rightTalon = new WPI_TalonSRX(1);
 
+    public void resetEncoder(){
+        leftTalon.setSelectedSensorPosition(0);
+        rightTalon.setSelectedSensorPosition(0)
+    }
 }
