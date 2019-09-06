f51a1c2f80826d9c12bbef91f27d9f87ce7c53fc
SameerRao <sameerrao22@gmail.com>
2019-09-06 16:32:29 -0700

    added move climber function

--- src/main/java/com/amhsrobotics/climber/ClimbSubsystem.java
+++ src/main/java/com/amhsrobotics/climber/ClimbSubsystem.java
@@ -1,6 +1,7 @@
 package com.amhsrobotics.climber;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -11,9 +12,24 @@ public class ClimbSubsystem extends Subsystem {
         return ourInstance;
     }
     WPI_TalonSRX rightTalon, leftTalon;
+    double tpi = 0;
+    double increment = 0;
     private ClimbSubsystem() {
+
+
+    }
+
+    public void initHardware(){
         WPI_TalonSRX leftTalon = new WPI_TalonSRX(0);
         WPI_TalonSRX rightTalon = new WPI_TalonSRX(1);
+        leftTalon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+        rightTalon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+        rightTalon.config_kP(0, 0);
+        rightTalon.config_kI(0, 0);
+        rightTalon.config_kD(0, 0);
+        leftTalon.config_kP(0, 0);
+        leftTalon.config_kI(0, 0);
+        leftTalon.config_kD(0, 0);
 
     }
 
@@ -44,5 +60,24 @@ public class ClimbSubsystem extends Subsystem {
         leftTalon.set(ControlMode.PercentOutput, 0);
         rightTalon.setSelectedSensorPosition(0);
         leftTalon.setSelectedSensorPosition(0);
+    }
+
+    public void moveClimber(double position) {
+        double setpoint = position*tpi;
+
+
+        while ((rightTalon.getSelectedSensorPosition() != setpoint) && (leftTalon.getSelectedSensorPosition() != setpoint)){
+            if (setpoint - rightTalon.getSelectedSensorPosition() >= increment) {
+                rightTalon.set(ControlMode.Position, rightTalon.getSelectedSensorPosition() + increment);
+            } else {
+                rightTalon.set(ControlMode.Position, setpoint);
+            }
+
+            if (setpoint - leftTalon.getSelectedSensorPosition() >= increment) {
+                leftTalon.set(ControlMode.Position, leftTalon.getSelectedSensorPosition() + increment);
+            } else {
+                leftTalon.set(ControlMode.Position, setpoint);
+            }
+        }
     }
 }
