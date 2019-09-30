f4d7cf664d1f50a9d06d05690a68760cffb256c1
Owen Leather <owenleather22@mittymonarch.com>
2019-09-29 23:19:02 -0700

    added turn to auton

--- src/main/java/com/amhsrobotics/autonomous/constants/AutoPaths.java
+++ src/main/java/com/amhsrobotics/autonomous/constants/AutoPaths.java
@@ -1,20 +1,17 @@
 package com.amhsrobotics.autonomous.constants;
 
 import com.amhsrobotics.autonomous.movement.PathProperties;
-import com.amhsrobotics.autonomous.movement.PathSequence;
 import com.amhsrobotics.purepursuit.Waypoint;
-
-import java.awt.geom.Point2D;
 
 public class AutoPaths {
 	public static PathProperties B_RIGHT_START_TO_FRONT_CS = new PathProperties(new Waypoint[]{AutoConstants.BLUE_RIGHT_START_POS, AutoConstants.BLUE_RIGHT_FRONT_HATCH}, 15, .8, 50, 50, 200,0,0,true,true, "B_RIGHT_END_HAB_TO_FRONT_CS");
-	public static PathProperties B_RIGHT_FRONT_CS_TO_LEFT_HELPER = new PathProperties(new Waypoint[]{AutoConstants.BLUE_RIGHT_FRONT_HATCH_REVERSED, AutoConstants.BLUE_RIGHT_HELPER_POINT}, 15, .8, 50, 50, 200,0,0,false,false, "B_RIGHT_FRONT_CS_TO_LEFT_HELPER");
-	public static PathProperties B_RIGHT_HELPER_TO_LEFT_LOADER = new PathProperties(new Waypoint[]{AutoConstants.BLUE_RIGHT_HELPER_POINT_TURN, AutoConstants.BLUE_RIGHT_LOADER_STATION}, 15, .8, 50, 50, 200,0,0,true,false, "B_RIGHT_HELPER_TO_LEFT_LOADER");
+	public static PathProperties B_RIGHT_FRONT_CS_TO_RIGHT_HELPER = new PathProperties(new Waypoint[]{AutoConstants.BLUE_RIGHT_FRONT_HATCH_REVERSED, AutoConstants.BLUE_RIGHT_HELPER_POINT}, 15, .8, 50, 50, 200,0,0,false,false, "B_RIGHT_FRONT_CS_TO_RIGHT_HELPER");
+	public static PathProperties B_RIGHT_HELPER_TO_RIGHT_LOADER = new PathProperties(new Waypoint[]{AutoConstants.BLUE_RIGHT_HELPER_POINT_TURN, AutoConstants.BLUE_RIGHT_LOADER_STATION}, 15, .8, 50, 50, 200,0,0,true,false, "B_RIGHT_HELPER_TO_RIGHT_LOADER");
 
 	public static PathProperties[] paths = new PathProperties[]{
 			B_RIGHT_START_TO_FRONT_CS,
-			B_RIGHT_FRONT_CS_TO_LEFT_HELPER,
-			B_RIGHT_HELPER_TO_LEFT_LOADER
+			B_RIGHT_FRONT_CS_TO_RIGHT_HELPER,
+			B_RIGHT_HELPER_TO_RIGHT_LOADER
 	};
 
 }
--- src/main/java/com/amhsrobotics/autonomous/modes/CargoShipFrontHatchAuto.java
+++ src/main/java/com/amhsrobotics/autonomous/modes/CargoShipFrontHatchAuto.java
@@ -2,13 +2,13 @@ package com.amhsrobotics.autonomous.modes;
 
 import com.amhsrobotics.autonomous.constants.AutoPaths;
 import com.amhsrobotics.autonomous.constants.AutoWaypoints;
+import com.amhsrobotics.autonomous.movement.commands.PIDRotation;
 import com.amhsrobotics.autonomous.movement.commands.Translate2dTrajectory;
 import com.amhsrobotics.autonomous.movement.commands.VisionAlignment;
 import com.amhsrobotics.autonomous.movement.commands.VisionAlignmentCargo;
 import com.amhsrobotics.drive.DriveTrain;
 import com.amhsrobotics.hatchpanel.commands.Grab;
 import com.amhsrobotics.hatchpanel.commands.PushBackward;
-import com.amhsrobotics.hatchpanel.commands.PushForward;
 import com.amhsrobotics.hatchpanel.commands.Release;
 import com.amhsrobotics.purepursuit.enums.PathType;
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -19,9 +19,6 @@ public class CargoShipFrontHatchAuto extends CommandGroup {
 		addSequential(new Grab());
 		if(stage == 0){
 			approachCargoShip();
-			visionPlace1();
-			driveToHelperPoint1();
-//			approachLoader();
 		}
 		else if(stage == 1){
 			visionPlace1();
@@ -34,15 +31,18 @@ public class CargoShipFrontHatchAuto extends CommandGroup {
 			approachLoader();
 		}
 		else if(stage == 4){
-			visionPickup();
+			turn1();
 		}
 		else if(stage == 5){
-			driveToHelperPoint2();
+			visionPickup();
 		}
 		else if(stage == 6){
-			approachRocket();
+			driveToHelperPoint2();
 		}
 		else if(stage == 7){
+			approachRocket();
+		}
+		else if(stage == 8){
 			visionPlace2();
 		}
 	}
@@ -52,11 +52,12 @@ public class CargoShipFrontHatchAuto extends CommandGroup {
 		approachCargoShip();
 		visionPlace1();
 		driveToHelperPoint1();
+		turn1();
 		approachLoader();
 		visionPickup();
-		driveToHelperPoint2();
-		approachRocket();
-		visionPlace2();
+//		driveToHelperPoint2();
+//		approachRocket();
+//		visionPlace2();
 	}
 
 	public void approachCargoShip(){
@@ -75,14 +76,18 @@ public class CargoShipFrontHatchAuto extends CommandGroup {
 	}
 	public void driveToHelperPoint1(){
 		System.out.println("Started drive to helper point 1 stage");
-		//addSequential(new Translate2dTrajectory(AutoWaypoints.BLUE_LEFT_HATCH_CARGOSHIP_HATCH_ROCKET[1], 50,50,200, PathType.CUBIC_HERMITE_PATH, 0,false, false));
-		addSequential( new Translate2dTrajectory(AutoPaths.B_RIGHT_FRONT_CS_TO_LEFT_HELPER));
+		addSequential( new Translate2dTrajectory(AutoPaths.B_RIGHT_FRONT_CS_TO_RIGHT_HELPER));
 		System.out.println("Ended drive to helper point 1 stage");
+	}
+	public void turn1(){
+		System.out.println("Started turn 1 stage");
+		//TODO: Figure out if 90 is positive or negative
+		addSequential(new PIDRotation(90));
+		System.out.println("Ended turn 1 stage");
 	}
 	public void approachLoader(){
 		System.out.println("Started approach loader stage");
-		//addSequential(new Translate2dTrajectory(AutoWaypoints.BLUE_RIGHT_HATCH_CARGOSHIP_HATCH_ROCKET[2], 10,1,50, PathType.CUBIC_HERMITE_PATH, 0, true, true));
-		addSequential( new Translate2dTrajectory(AutoPaths.B_RIGHT_HELPER_TO_LEFT_LOADER));
+		addSequential( new Translate2dTrajectory(AutoPaths.B_RIGHT_HELPER_TO_RIGHT_LOADER));
 		System.out.println("Ended approach loader stage");
 	}
 	public void visionPickup(){
