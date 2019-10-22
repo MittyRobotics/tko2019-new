8a8c44dbd1a437445c24537930c930b8385794de
programming1351 <programming@amhsrobotics.com>
2019-10-22 15:05:14 -0700

    Talon Inversions

--- src/main/java/com/amhsrobotics/drive/constants/TalonInversions.java
+++ src/main/java/com/amhsrobotics/drive/constants/TalonInversions.java
@@ -1,6 +1,6 @@
 package com.amhsrobotics.drive.constants;
 
 public class TalonInversions {
-	public static final boolean[] LEFT_DRIVE = {false, false};
-	public static final boolean[] RIGHT_DRIVE = {true, true};
+	public static final boolean[] LEFT_DRIVE = {true, true};
+	public static final boolean[] RIGHT_DRIVE = {false, false};
 }
